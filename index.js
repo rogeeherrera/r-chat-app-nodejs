c6cd61ff90a22dbfc7a354ffc895a9dddf75bbaa
Rogee Herrera <rogeeherrera@gmail.com>
2019-02-14 17:29:23 +0800

    ~updated to broadcast messages to all user

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.get('/', function(req,res){
 
 io.on('connection',function(socket){
     socket.on('chat message',function(msg){
-        console.log('Message: '+ msg);
+        io.emit('chat message', msg);
     });
-    console.log('A User has connected');
 });
 
 http.listen(3000,function(){
